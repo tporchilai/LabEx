be5d790560ecd6a6084b445d301f3cbdf364469c
tporchilai <tporchilai@gmail.com>
2024-05-18 21:10:27 +0530

    continue condition using if

---
+++ src/arithmetic.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the operators");
+        char c= sc.next().charAt(0);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        double result;
+
+
+        switch(c)
+        {
+            case '+':
+                result = num1 + num2;
+                System.out.print(result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println(result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println(result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println(result);
+                } else {
+                    System.out.println("Error! Division by zero.");
+                }
+                break;
+            default:
+                System.out.println("Error! Invalid operator.");
+
+        }
+
+    }
+
+}
---
+++ src/countinecondition.java
@@ -0,0 +1,13 @@
+public class countinecondition {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++)
+        {
+            System.out.println(i);
+            if(i==5)
+            {
+                continue;
+            }
+            System.out.println("After");
+        }
+    }
+}
